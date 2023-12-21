206fed240364e921b6143a8b4418f463667fc0b9
moyotsukai <gyoshu.bowie@gmail.com>
2023-12-21 11:21:34 +0900

    Initial commit

---
+++ TechMon/BattleViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BattleViewController.swift
+//  TechMon
+//
+//  Created by Owner on 2023/12/21.
+//
+
+import UIKit
+
+class BattleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ TechMon/LobbyViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LobbyViewController.swift
+//  TechMon
+//
+//  Created by Owner on 2023/12/21.
+//
+
+import UIKit
+
+class LobbyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
